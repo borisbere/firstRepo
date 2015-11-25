54d4919ffa57c05d92effecd078bb9433ded1a12
borisbere <borisbere18@yahoo.com.ar>
2015-11-25 13:10:26 -0300

    topic 01 complete

--- 01-js-intro-and-jquery/index.js
+++ 01-js-intro-and-jquery/index.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
-$("div").fadeIn("slow");
-  	  $("button").click(function(){
-     	  try{
-  	         $.get("http://bootcamp.aws.af.cm/welcome/Boris",function(response){
-  	       	    $("div").text(JSON.stringify(response).substring(13,27))})
-  	      }catch(err){
+      $("div").fadeIn("slow");
+  	  $("#search").click(function(){
+     	 try{
+  	         $.get("http://bootcamp.aws.af.cm/welcome/Boris",function(data){
+    	       	    $("div").text(data.response)
+             })
+       }catch(err){
   	         $("div").text("server error");
   	            $("section").css("color","#FF0000");}
         $("div").css("color","#FF0000"); });
-  	    $(".alias").focus();
-        $.get("http://ws.spotify.com/search/1/album?q=Rolling%20Stones",function(data){
-  	    	$(".albums").text(JSON.stringify(data));/*  -----THIS FUNCTION DOESN'T WORK------*/
-        /* 
-        var xmlhttp = new XMLHttpRequest()  
-        var arr = json.parse(xmlhttp.responseText); 
-        xmlhttp.open("GET", "http://ws.spotify.com/search/1/album?q=Rolling%20Stones", true);
-        xmlhttp.send();
-       */;
-  	    });
+  	    $(".alias").focus(); 
+        
+        $("#search").click(function(){ 
+        var p = $(".alias").val();  
+        $.get("https://api.spotify.com/v1/search?q="+p+"&type=album",function(data){
+            $(".albums").text("")
+            for (var i = 0; i < 100; i++) {
+            $(".albums").append("<img src="+data.albums.items[i].images[2].url+" />");   
+            $(".albums").append(data.albums.items[i].album_type+"......");      
+            $(".albums").append("title: "+"<b>"+data.albums.items[i].name+"</b>"+"......");
+            var address = data.albums.items[i].external_urls.spotify;
+            $(".albums").append("<a href="+address+">Listen Now</a><br>")       
+      
+          }
+            })})
+  	       
   	})
