e1a2692e42344a21fb129b5271ed7e0ba8d047e5
borisbere <borisbere18@yahoo.com.ar>
2015-12-01 13:38:12 -0300

    topic 02 complete

--- 01-js-intro-and-jquery/index.js
+++ 01-js-intro-and-jquery/index.js
@@ -12,17 +12,17 @@ $(document).ready(function(){
   	    $(".alias").focus(); 
         
         $("#search").click(function(){ 
-        var p = $(".alias").val();  
-        $.get("https://api.spotify.com/v1/search?q="+p+"&type=album",function(data){
+          var p = $(".alias").val();  
+          $.get("https://api.spotify.com/v1/search?q="+p+"&type=album",function(data){
             $(".albums").text("")
             for (var i = 0; i < 100; i++) {
-            $(".albums").append("<img src="+data.albums.items[i].images[2].url+" />");   
-            $(".albums").append(data.albums.items[i].album_type+"......");      
-            $(".albums").append("title: "+"<b>"+data.albums.items[i].name+"</b>"+"......");
-            var address = data.albums.items[i].external_urls.spotify;
-            $(".albums").append("<a href="+address+">Listen Now</a><br>")       
+               $(".albums").append("<img src="+data.albums.items[i].images[2].url+" />");   
+               $(".albums").append(data.albums.items[i].album_type+"......");      
+               $(".albums").append("title: "+"<b>"+data.albums.items[i].name+"</b>"+"......");
+               var address = data.albums.items[i].external_urls.spotify;
+               $(".albums").append("<a href="+address+">Listen Now</a><br>")       
       
-          }
-            })})
-  	       
-  	})
+            }
+          })
+        })
+})
