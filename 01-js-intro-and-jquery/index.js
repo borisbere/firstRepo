6791c015dde5860ada17bf551ad05d67ab0407ce
borisbere <borisbere18@yahoo.com.ar>
2015-12-11 13:17:57 -0300

    Starting Proyext

--- 01-js-intro-and-jquery/index.js
+++ 01-js-intro-and-jquery/index.js
@@ -2,9 +2,15 @@ $(document).ready(function(){
       $("div").fadeIn("slow");
   	  $("#search").click(function(){
      	 try{
-  	         $.get("http://bootcamp.aws.af.cm/welcome/Boris",function(data){
-    	       	    $("div").text(data.response)
-             })
+            $.ajax({
+              url: "http://bootcamp.aws.af.cm/welcome/Boris",
+              success: function(data){
+                  $("div").text(data.response) 
+            }
+            });
+  	         /*$.get("http://bootcamp.aws.af.cm/welcome/Boris",function(data){
+    	       	    $("div").text(data.response)*/
+             
        }catch(err){
   	         $("div").text("server error");
   	            $("section").css("color","#FF0000");}
@@ -12,8 +18,10 @@ $(document).ready(function(){
   	    $(".alias").focus(); 
         
         $("#search").click(function(){ 
-          var p = $(".alias").val();  
-          $.get("https://api.spotify.com/v1/search?q="+p+"&type=album",function(data){
+          var p = $(".alias").val();
+          $.ajax({
+              url:"https://api.spotify.com/v1/search?q="+p+"&type=album",
+              success: function(data){
             $(".albums").text("")
             for (var i = 0; i < 100; i++) {
                $(".albums").append("<img src="+data.albums.items[i].images[2].url+" />");   
@@ -23,6 +31,18 @@ $(document).ready(function(){
                $(".albums").append("<a href="+address+">Listen Now</a><br>")       
       
             }
-          })
+               }});
+
+         /* $.get("https://api.spotify.com/v1/search?q="+p+"&type=album",function(data){
+            $(".albums").text("")
+            for (var i = 0; i < 100; i++) {
+               $(".albums").append("<img src="+data.albums.items[i].images[2].url+" />");   
+               $(".albums").append(data.albums.items[i].album_type+"......");      
+               $(".albums").append("title: "+"<b>"+data.albums.items[i].name+"</b>"+"......");
+               var address = data.albums.items[i].external_urls.spotify;
+               $(".albums").append("<a href="+address+">Listen Now</a><br>")       
+      
+            }
+          })*/
         })
 })
---
+++ Proyect/WeatherMaster.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+      $(".intro").fadeOut("slow");
+      $(".search").click(function(){
+        var city = $("#city").val();
+        var province = $("#province").val();
+        $.ajax({
+              url:"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%E2%80%8B%2C%20"+province+"%E2%80%8B%22)%20and%20u=%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+              success: function(data){
+                var weather = data.query.results.channel;
+                $(".results, .resTem, .subRes, .resIco, .resUni, .resPre, .resHum, .resWin").text("");
+                $(".results").append(weather.location.city+", ");
+                $(".results").append(weather.location.country+"<br>");
+                $(".results").append(weather.item.condition.date+"<br>");
+                $(".results").append(weather.item.condition.text+"<br><br>");
+                var code = weather.item.condition.code;
+
+                $(".resIco").append("<img src=\http://l.yimg.com/a/i/us/we/52/"+code+".gif\ />");
+                $(".resTem").append(weather.item.condition.temp);
+                $(".resUni").append("°"+weather.units.temperature);
+                $(".resPre").append("Pressure: "+weather.atmosphere.pressure+" "+weather.units.pressure);
+                $(".resHum").append("Humidity: "+weather.atmosphere.humidity+"%");
+                $(".resWin").append("Wind: "+weather.wind.speed+" "+weather.units.speed);
+              }
+
+              })
+      })
+})      
+  	  /*$("#search").click(function(){
+     	 try{
+            $.ajax({
+              url: "http://bootcamp.aws.af.cm/welcome/Boris",
+              success: function(data){
+                  $("div").text(data.response) 
+            }
+            });
+
+             
+       }catch(err){
+  	         $("div").text("server error");
+  	            $("section").css("color","#FF0000");}
+        $("div").css("color","#FF0000"); });
+  	    $(".alias").focus(); 
+        
+        
+               }});
+        })*/
