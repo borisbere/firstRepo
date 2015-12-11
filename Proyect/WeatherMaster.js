267179c53f9feca9e1b3776fdf20c22cc1222238
borisbere <borisbere18@yahoo.com.ar>
2015-12-11 15:23:03 -0300

    Proyect changes

--- Proyect/WeatherMaster.js
+++ Proyect/WeatherMaster.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
               url:"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city+"%E2%80%8B%2C%20"+province+"%E2%80%8B%22)%20and%20u=%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
               success: function(data){
                 var weather = data.query.results.channel;
+                $(".res").css("display","block");
                 $(".results, .resTem, .subRes, .resIco, .resUni, .resPre, .resHum, .resWin").text("");
                 $(".results").append(weather.location.city+", ");
                 $(".results").append(weather.location.country+"<br>");
