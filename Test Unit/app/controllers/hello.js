1071cc787891c827bfd0a25fe36f2f6f4bd1c158
borisbere <borisbere18@yahoo.com.ar>
2016-01-20 14:55:14 -0300

    more functions added to test unit

--- Test Unit/app/controllers/hello.js
+++ Test Unit/app/controllers/hello.js
@@ -3,4 +3,8 @@ angular.module("app", [])
 .controller("helloCtrl", function($scope)
 {
    $scope.hello = "";
+   $scope.hello1 = '5';
+   $scope.hello2 = true;
+   $scope.hello3 = 10;
+   $scope.hello4 = 15;
 })
--- Test Unit/tests/controllers/hello.js
+++ Test Unit/tests/controllers/hello.js
@@ -14,7 +14,25 @@ describe("probando controlador hello", function()
  
         it("debe estar definida una variable hello", function()
         {
+            var a = 15;
+            var b = null;
+            var array = [5, 'the', 21];
+            var message = "hi globant bootcamp"
             expect(scope.hello).toBeDefined();
+            expect(scope.hello1).toBe('5');
+            expect(scope.hello2).toBe(true);
+            expect(false).not.toBe(true);
+            expect(scope.hello3).toEqual(10);
+            expect(a).toEqual(scope.hello4);
+            expect(message).toMatch(/globant/);
+            expect(message).toMatch("bootcamp");
+            expect(message).not.toMatch("hello");
+            expect(a).not.toBeUndefined;
+            expect(b).toBeNull;
+            expect(a).toBeTruthy();
+            expect(message).not.toBeFalsy();
+            expect(array).toContain('the');
+            expect(a).toBeLessThan(20);
         })
     })
 })
